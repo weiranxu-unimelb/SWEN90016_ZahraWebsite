58b45c764bfd141569f105e41b39e50ed1af185b
weiran xu <weiranx1@student.unimelb.edu.au>
2023-10-01 05:12:45 +1100

    User Case 5,6,7 have been completed.

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const multer = require('multer');
 const mongoose = require('mongoose');
+const path = require('path');
 
 // Initialize the app.
 const app = express();
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-// DB connect code for 'mongoose' package
+// DB connect code for 'mongoose' package.
 const uri = "mongodb+srv://weiranx1:weiranxu123@swen90016.kohjae3.mongodb.net/Zahra_Data?retryWrites=true&w=majority";
 mongoose.connect(uri, {
     useNewUrlParser: true,
@@ -15,7 +17,7 @@ mongoose.connect(uri, {
 
 // Get the connection status.
 var connection = mongoose.connection;
-connection.on('connected', function(err){
+connection.on('connected', function (err) {
     if (err) console.log('Bad connection: ' + err);
     else console.log('Successfully connect to the database.');
 });
@@ -35,7 +37,15 @@ const customerSchema = mongoose.Schema({
 });
 const Customer = mongoose.model('Customer', customerSchema);
 // Set Multer
-const upload = multer({ dest: 'uploads/PDF/' });
+const storage_PDF = multer.diskStorage({
+    destination: 'uploads/PDF/',
+    filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname);
+        const fileName = file.originalname.replace(ext, '') + Date.now() + ext;
+        cb(null, fileName);
+    }
+});
+const uploadPDF = multer({ storage: storage_PDF, });
 // Set Ejs
 app.set('view engine', 'ejs');
 // Router
@@ -47,7 +57,7 @@ app.get('/customer', (req, res) => {
     res.render('customer');
 });
 
-app.post('/submit', upload.single('identifyingDocument'), (req, res) => {
+app.post('/submit', uploadPDF.single('identifyingDocument'), (req, res) => {
     const customer = new Customer({
         name: req.body.name,
         age: req.body.age,
@@ -68,6 +78,182 @@ app.post('/submit', upload.single('identifyingDocument'), (req, res) => {
             console.log('Failed to save customer details', error);
             res.render('customer_error');
         });
+});
+
+// Usercase 5,6,7.
+// Create CarpetCategory schema.
+const carpetCategorySchema = new mongoose.Schema({
+    name: String,
+});
+
+// Create CarpetItem schema.
+const carpetItemSchema = new mongoose.Schema({
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'CarpetCategory'
+    },
+    length: Number,
+    breadth: Number,
+    weight: Number,
+    dimensions: String,
+    colour: String,
+    pattern: String,
+    dominantColour1: String,
+    dominantColour2: String,
+    images: [String],
+    size: {
+        type: String,
+        enum: ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+    },
+    numericSize: Number,
+    quantity: Number,
+    countryOfOrigin: String
+});
+
+// Create CarpetKitItem schema.
+const carpetKitItemSchema = new mongoose.Schema({
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'CarpetCategory'
+    },
+    length: Number,
+    breadth: Number,
+    weight: Number,
+    dimensions: String,
+    images: [String],
+    quantity: Number,
+    countryOfPackaging: String
+});
+
+
+// Create model.
+const CarpetCategory = mongoose.model('CarpetCategory', carpetCategorySchema);
+const CarpetItem = mongoose.model('CarpetItem', carpetItemSchema);
+const CarpetKitItem = mongoose.model('CarpetKitItem', carpetKitItemSchema);
+
+// Usercase 5 -> Create carpet category.
+app.get('/carpetCategories', (req, res) => {
+    res.render('carpetCategories');
+});
+
+app.post('/carpet-category', async (req, res) => {
+    const carpetCategory = new CarpetCategory({
+        name: req.body.name
+    });
+
+    carpetCategory.save()
+        .then(() => {
+            res.render('carpetCategories_success');
+        })
+        .catch((error) => {
+            console.log('Failed to save carpet category details', error);
+            res.render('carpetCategories_error');
+        });
+});
+
+// Usercase 6 -> Create carpet item.
+// Get all carpet categories.
+app.get('/carpetItems', async (req, res) => {
+    try {
+        const carpetCategories = await CarpetCategory.find();
+        res.render('carpetItems', {
+            carpetCategories: carpetCategories
+        });
+    } catch (error) {
+        console.log('Failed to retrieve carpet categories', error);
+        res.render('carpetKitItems_error');
+    }
+});
+
+const storage_item = multer.diskStorage({
+    destination: 'uploads/PNG_item/',
+    filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname);
+        const fileName = file.originalname.replace(ext, '') + Date.now() + ext;
+        cb(null, fileName);
+    }
+});
+
+const uploadPNG_item = multer({ storage: storage_item, });
+
+app.post('/carpet-item', uploadPNG_item.array('images'), async (req, res) => {
+    try {
+        const files = req.files;
+        const filePaths = files.map(file => file.path + path.extname(file.originalname));
+        const carpetItem = new CarpetItem({
+            category: req.body.category,
+            length: req.body.length,
+            breadth: req.body.breadth,
+            weight: req.body.weight,
+            dimensions: req.body.dimensions,
+            colour: req.body.colour,
+            pattern: req.body.pattern,
+            dominantColour1: req.body.dominantColour1,
+            dominantColour2: req.body.dominantColour2,
+            images: filePaths,
+            size: req.body.size,
+            numericSize: req.body.numericSize,
+            quantity: req.body.quantity,
+            countryOfOrigin: req.body.countryOfOrigin
+        });
+
+        await carpetItem.save();
+        res.render('carpetItems_success');
+    } catch (error) {
+        console.log('Failed to save carpet item details', error);
+        res.render('carpetItems_error');
+    }
+});
+
+// Usercase 7 -> Create carpet kits.
+app.get('/carpetKitItems', async (req, res) => {
+    try {
+        const carpetCategories = await CarpetCategory.find();
+        res.render('carpetKitItems', {
+            carpetCategories: carpetCategories
+        });
+    } catch (error) {
+        console.log('Failed to retrieve carpet categories', error);
+        res.render('carpetKitItems_error');
+    }
+});
+
+const storage_kit = multer.diskStorage({
+    destination: 'uploads/PNG_kit/',
+    filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname);
+        const fileName = file.originalname.replace(ext, '') + Date.now() + ext;
+        cb(null, fileName);
+    }
+});
+
+const uploadPNG_kit = multer({ storage: storage_kit, });
+
+app.post('/carpet-kit-item', uploadPNG_kit.array('images'), async (req, res) => {
+    const files = req.files;
+    const filePaths = files.map(file => file.path);
+    const carpetKitItems = new CarpetKitItem({
+        category: req.body.category,
+        length: req.body.length,
+        breadth: req.body.breadth,
+        weight: req.body.weight,
+        dimensions: req.body.dimensions,
+        images: req.filePaths,
+        size: req.body.size,
+        numericSize: req.body.numericSize,
+        quantity: req.body.quantity,
+        countryOfOrigin: req.body.countryOfOrigin
+    });
+
+    carpetKitItems.save()
+        .then(() => {
+            res.render('carpetKitItems_success');
+        })
+        .catch((error) => {
+            console.log('Failed to save carpet kit details', error);
+            res.render('carpetKitItems_error');
+        });
+
 });
 
 
